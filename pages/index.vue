aa9c0c070f125cc350d49d361e4384301bbfa5f2
yuu-eguci <yuu.eguci@gmail.com>
2023-04-29 17:41:40 +0900

    ♻️ コンポーネント化

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,16 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-text-field
-        v-model="utcInput"
-        :counter="20"
-        label="UTC"
-        outlined
-        style="font-size: 1.3rem; padding: 0.5rem 0;"
+      <main-component
+        :from-tz="'UTC'"
+        :to-tz="'Asia/Tokyo'"
       />
-      <v-card
-        :ripple="false"
-        @click="copyUtcConverted"
-      >
-        <v-card-subtitle>
-          JST
-        </v-card-subtitle>
-        <v-card-text
-          class="d-flex align-center justify-center font-weight-bold"
-          style="font-size: 1.3rem; padding: 0.5rem 0;"
-        >
-          {{ utcConverted }}
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <template
-            v-if="utcConvertedCopied"
-          >
-            {{ $t('indexPage.copied') }}
-          </template>
-          <v-icon
-            fab
-            class="ml-2"
-            small
-          >
-            mdi-content-copy
-          </v-icon>
-        </v-card-actions>
-      </v-card>
+
+      <main-component
+        :from-tz="'Asia/Tokyo'"
+        :to-tz="'UTC'"
+      />
+
       <v-card
         class="mt-4"
       >
@@ -73,6 +47,7 @@
         </v-card-actions>
       </v-card>
     </v-col>
+
     <v-overlay
       opacity="0.5"
       color="grey"
@@ -91,37 +66,17 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import * as dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
-
-dayjs.extend(utc)
-dayjs.extend(timezone)
+import MainComponent from '@/components/MainComponent.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  components: {
+    MainComponent
+  },
   data () {
     return {
-      innerUtcInput: '',
-      utcConverted: '',
-      utcConvertedCopied: false,
       showOverlay: false,
       animation: false
-    }
-  },
-  computed: {
-    utcInput: {
-      get () {
-        return this.innerUtcInput
-      },
-      set (value: string) {
-        this.innerUtcInput = value
-        try {
-          this.utcConverted = (dayjs as any).tz(value, 'utc').tz('Asia/Tokyo').format()
-        } catch (err: unknown) {
-          this.utcConverted = (err as any).message
-        }
-      }
     }
   },
   mounted () {
@@ -140,18 +95,6 @@ export default Vue.extend({
           this.showOverlay = false
         }, 1000)
       }, 1000)
-    },
-    async copyUtcConverted () {
-      try {
-        this.$debug(`Copy text: ${this.utcConverted}`)
-        await navigator.clipboard.writeText(this.utcConverted)
-        this.utcConvertedCopied = true
-        setTimeout(() => {
-          this.utcConvertedCopied = false
-        }, 3000)
-      } catch (err) {
-        this.$debug(`Failed to copy text: ${this.utcConverted}`)
-      }
     }
   }
 })
